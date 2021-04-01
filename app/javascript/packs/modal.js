09e1776b0cea01dca6e658d0caef1e497628b3fe
Eric KUOCH <ek.kuoch@gmail.com>
2021-04-01 18:52:45 +0200

    cleaning

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,13 +1,11 @@
 document.addEventListener("turbolinks:load", function() {
   const triggersModal = document.querySelectorAll('[data-trigger-modal]')
   const modal = document.querySelector('[data-modal]')
-  const closeBtn = document.querySelector('.close')
   if (triggersModal) {
     triggersModal.forEach(trigger => {
       trigger.addEventListener("click", () => {
         modal.style.display = "block";
       })
-     
       window.addEventListener("click", (event) => {
         if (event.target == modal) {
           modal.style.display = "none";
