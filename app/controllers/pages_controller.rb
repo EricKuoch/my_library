e1836a7ac5d4bdc03ed4f99b67c5be8cca786a30
Eric KUOCH <ek.kuoch@gmail.com>
2021-04-09 16:37:43 +0200

    stats for chart.js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,9 @@ class PagesController < ApplicationController
   end
   
   def stats
-    @stats_library_books = current_user.library.library_books.group_by_day(:created_at, format: "%Y-%m-%d",  last: 7).count
-    @stats_quotes = Quote.where(user_id:current_user).group_by_day(:created_at, format: "%Y-%m-%d",  last: 7).count
-    @library_books = Quote.where(user_id:current_user).group_by_day(:created_at, format: "%Y-%m-%d",  last: 7).count
+    @stats_library_books_by_week = current_user.library_books.group_by_day(:created_at, format: "%Y-%m-%d",  last: 7).count
+    @stats_quotes_by_week = Quote.where(user_id:current_user).group_by_day(:created_at, format: "%Y-%m-%d",  last: 7).count
+    @library_books_total = current_user.library_books.count
+    @quotes_total = current_user.quotes.count
   end
 end
