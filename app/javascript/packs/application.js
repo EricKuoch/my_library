a105e95d549b86f2dd79776728efa1902da6319b
Eric KUOCH <ek.kuoch@gmail.com>
2021-04-12 23:37:47 +0200

    add js for delete button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,10 @@ require("packs/modal")
 require("packs/status")
 require("packs/chart")
 require("packs/marvel_api")
+require("packs/launcher")
+require("packs/delete_book")
 const Chart = require('chart.js')
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
