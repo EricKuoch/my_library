fcb3c2f670c86ffb0b20198d5a94ea5eea3617af
Eric KUOCH <ek.kuoch@gmail.com>
2021-04-12 23:33:45 +0200

    add some validations

--- app/models/library_book.rb
+++ app/models/library_book.rb
@@ -1,8 +1,13 @@
 class LibraryBook < ApplicationRecord
-  has_many :quotes
+  has_many :quotes, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :quotes
   enum status: { bought: 0, reading: 1, finished: 2}
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :status, presence: true
 
   scope :reading, -> { where(status: "reading") }
   scope :bought, -> { where(status: "bought") }
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,4 @@
 class Quote < ApplicationRecord
   belongs_to :library_book
+  validates :description, presence: true
 end
